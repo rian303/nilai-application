b2eb8fee4fc3490a6aabf998247d290a18510e26
rian303 <zotech303@gmail.com>
2025-06-12 02:58:59 +0700

    change index.js

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+// Module Database
+const db = require("./config/db");
+
+// Route
+const studentRoute = require("./routes/studentRoute");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: true,
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+app.use(bodyParser.json());
+
+app.use("/api/nilai-app", studentRoute);
+
+app.get("/", (req, res) => {
+  return res.json("Hello World");
+});
+
+// Database connecting
+db.getConnection().then(() => {
+  app.listen(PORT, async (err) => {
+    if (err) return console.log("[-] Server Error: " + err);
+
+    return console.log(`[+] Server successfully connecting on port: ${PORT}`);
+  });
+});
